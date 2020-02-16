64f408d9529839063a24fe086395a4a08cc45a9e
Isabella Beatriz <isabellabeatrizn@outlook.com>
2020-02-16 19:26:08 -0300

    Adding monochrome to ColorPalette

--- charts/lib/color_palette.dart
+++ charts/lib/color_palette.dart
@@ -5,31 +5,42 @@ import 'package:flutter/material.dart';
 class ColorPalette {
   static final ColorPalette primary = new ColorPalette(<Color>[
     Colors.blue[400],
-    Colors.blue[200],
     Colors.red[400],
-    Colors.red[200],
     Colors.green[400],
-    Colors.green[200],
     Colors.yellow[400],
-    Colors.yellow[200],
     Colors.purple[400],
-    Colors.purple[200],
     Colors.orange[400],
-    Colors.orange[200],
     Colors.teal[400],
-    Colors.teal[200],
-    Colors.black,
   ]);
 
   ColorPalette(List<Color> colors) : _colors = colors {
     assert(colors.isNotEmpty);
   }
 
+  factory ColorPalette.monochrome(Color base, int length) {
+    return new ColorPalette(new List.generate(
+      length,
+      (i) => _brighterColor(base, i, length),
+    ));
+  }
+
+  static Color _brighterColor(Color base, int i, int n) {
+    return new Color.fromARGB(
+        base.alpha,
+        _brighterComponent(base.red, i, n),
+        _brighterComponent(base.green, i, n),
+        _brighterComponent(base.blue, i, n));
+  }
+
+  static int _brighterComponent(int base, int i, int n) {
+    return (base + i * (255 - base) / n).floor();
+  }
+
   final List<Color> _colors;
 
-  Color operator [](int index) => _colors[index % length];
+  Color operator [](int index) => _colors[index % _colors.length];
 
   int get length => _colors.length;
 
-  Color random(Random random) => this[random.nextInt(length)];
+  Color random(Random random) => this[random.nextInt(_colors.length)];
 }

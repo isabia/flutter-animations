edfad6d2baffe1e2dad39df699da8cc495e46a00
Isabella Beatriz <isabellabeatrizn@outlook.com>
2020-02-16 19:34:34 -0300

    Setting BarPainter to paint grouped bars with diferent colors

--- charts/lib/barchartpainter.dart
+++ charts/lib/barchartpainter.dart
@@ -1,10 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'bar.dart';
 
 class BarChartPainter extends CustomPainter {
-  static const barWidthFraction = 0.75;
-
   BarChartPainter(Animation<BarChart> animation)
       : animation = animation,
         super(repaint: animation);
@@ -13,22 +13,36 @@ class BarChartPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    void drawBar(Bar bar, double x, double width, Paint paint) {
-      paint.color = bar.color;
-      canvas.drawRect(
-        Rect.fromLTWH(x, size.height - bar.height, width, bar.height),
-        paint,
-      );
-    }
-
-    final paint = Paint()..style = PaintingStyle.fill;
+    final barPaint = Paint()..style = PaintingStyle.fill;
+    final linePaint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.white
+      ..strokeWidth = 1.0;
+    final linePath = Path();
     final chart = animation.value;
-    final barDistance = size.width / (1 + chart.bars.length);
-    final barWidth = barDistance * barWidthFraction;
-    var x = barDistance - barWidth / 2;
-    for (final bar in chart.bars) {
-      drawBar(bar, x, barWidth, paint);
-      x += barDistance;
+    for (final group in chart.groups) {
+      for (final stack in group.stacks) {
+        var y = size.height;
+        for (final bar in stack.bars) {
+          barPaint.color = bar.color;
+          canvas.drawRect(
+            Rect.fromLTWH(
+              stack.x,
+              y - bar.height,
+              stack.width,
+              bar.height,
+            ),
+            barPaint,
+          );
+          if (y < size.height) {
+            linePath.moveTo(stack.x, y);
+            linePath.lineTo(stack.x + stack.width, y);
+          }
+          y -= bar.height;
+        }
+        canvas.drawPath(linePath, linePaint);
+        linePath.reset();
+      }
     }
   }
 
